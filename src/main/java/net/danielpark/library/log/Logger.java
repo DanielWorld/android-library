8ff459621e8a1cad9468b9f9ce72a04dbc9b89e6
Daniel Park <op7773hons@gmail.com>
2017-11-08 14:53:40 +0900

    Improve Logger - Represent Log's position in detail.

--- src/main/java/net/danielpark/library/log/Logger.java
+++ src/main/java/net/danielpark/library/log/Logger.java
@@ -10,45 +10,86 @@ import net.danielpark.library.BuildConfig;
 
 public final class Logger {
 
-    private static Logger mInstance;
-
-    public static synchronized Logger getInstance() {
-        if (mInstance == null)
-            mInstance = new Logger();
-        return mInstance;
-    }
-
-    public Logger() {}
-
     private boolean mLogFlag = BuildConfig.DEBUG;
+
+    /**
+     * This is useful format for Logging. <br>
+     *     Represents that log's position in detail.
+     * @param message
+     * @return
+     */
+    private static String buildLogMsg(String message) {
+        StackTraceElement ste = Thread.currentThread().getStackTrace()[4];
+        StringBuilder sb = new StringBuilder();
+        sb.append("[[");
+        sb.append(ste.getFileName());
+        sb.append(">");
+        sb.append(ste.getMethodName());
+        sb.append(">#");
+        sb.append(ste.getLineNumber());
+        sb.append("]] ");
+        sb.append(message);
+
+        return sb.toString();
+    }
 
     public void v(String TAG, String msg) {
         if (mLogFlag) {
-            Log.v(TAG, msg);
+            Log.v(TAG, buildLogMsg(msg));
         }
     }
 
     public void d(String TAG, String msg) {
         if (mLogFlag) {
-            Log.d(TAG, msg);
+            Log.d(TAG, buildLogMsg(msg));
         }
     }
 
     public void e(String TAG, String msg) {
         if (mLogFlag) {
-            Log.e(TAG, msg);
+            Log.e(TAG, buildLogMsg(msg));
         }
     }
 
     public void i(String TAG, String msg) {
         if (mLogFlag) {
-            Log.i(TAG, msg);
+            Log.i(TAG, buildLogMsg(msg));
         }
     }
 
     public void w(String TAG, String msg) {
         if (mLogFlag) {
-            Log.w(TAG, msg);
+            Log.w(TAG, buildLogMsg(msg));
+        }
+    }
+
+    public void v(String TAG, String msg, Throwable throwable) {
+        if (mLogFlag) {
+            Log.v(TAG, buildLogMsg(msg), throwable);
+        }
+    }
+
+    public void d(String TAG, String msg, Throwable throwable) {
+        if (mLogFlag) {
+            Log.d(TAG, buildLogMsg(msg), throwable);
+        }
+    }
+
+    public void e(String TAG, String msg, Throwable throwable) {
+        if (mLogFlag) {
+            Log.e(TAG, buildLogMsg(msg), throwable);
+        }
+    }
+
+    public void i(String TAG, String msg, Throwable throwable) {
+        if (mLogFlag) {
+            Log.i(TAG, buildLogMsg(msg), throwable);
+        }
+    }
+
+    public void w(String TAG, String msg, Throwable throwable) {
+        if (mLogFlag) {
+            Log.w(TAG, buildLogMsg(msg), throwable);
         }
     }
 }
